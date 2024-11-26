0fddf3d9269e8beb3f46646d169ad6ed4f35ed7c
Night <146769399+Jakkarin-Promsee@users.noreply.github.com>
2024-11-27 04:13:19 +0700

    Feature: Check point 4.

--- Assets/Scripts/Boss/Spirit King.cs
+++ Assets/Scripts/Boss/Spirit King.cs
@@ -9,23 +9,7 @@ public class SpiritKing : MonoBehaviour
     public float moveFrictionCoefficient = 1;
 
 
-    // Circle status
-    public float circleCD = 6;
-    public float circleDuration = 2;
-    public float circleSpeed = 3;
-    public float circleAngularSpeed = 0.3f;
-
-    public float circleRadius = 3;
-    public bool _forceFixedAtCircle = false;
-
-    ///////////////////////////////////////
-    private bool _forceEnd = false;
-    private bool _nextMovementState = true;
-    private float _circleFaceAngle = 0f;
-    private float _circleOffsetRadius = 0f;
-    private bool _isCircleCW = true;
-
-    private enum State { Idle, ChaseCircle, ChaseStraight, Random };
+    private enum State { Idle, Chase, Random };
     private enum Attack { NormalAttack, Skill1, Skill2, Skill3, Skill4, Skill5, Skill6 }
     /*
         Idle => Rest Stand
@@ -57,11 +41,24 @@ public class SpiritKing : MonoBehaviour
     {
         ApplyFriction();
 
-        switch (_currentState)
+        if (_isAttack)
         {
-            case State.Idle: Idle(); break;
-            case State.ChaseCircle: ChaseCircle(); break;
+            switch (_currentAttack)
+            {
+                case Attack.NormalAttack: NormalAttack(); break;
+            }
         }
+        else
+        {
+            switch (_currentState)
+            {
+                case State.Idle: Idle(); break;
+                case State.Chase: break;
+            }
+        }
+
+
+
     }
 
     private void ApplyFriction()
@@ -73,78 +70,48 @@ public class SpiritKing : MonoBehaviour
 
     private void Idle()
     {
-        if (_nextNormalAttack)
+        if (_nextNomalAttack) NormalAttack();
+    }
+
+    private void NormalAttack()
+    {
+        _currentAttack = Attack.NormalAttack;
+        _isAttack = true;
+        _nextNomalAttack = false;
+
+        if (!ChasePlayer(1f))
+            return;
+
+        Debug.Log("I got you");
+        _isAttack = false;
+        StartCoroutine(CountNormalAttackCD());
+    }
+
+    IEnumerator CountNormalAttackCD()
+    {
+        _nextNomalAttack = false;
+        yield return new WaitForSeconds(NormalAttackCD);
+        _nextNomalAttack = true;
+    }
+
+
+    public float moveSpeed = 3.5f;
+    public float NormalAttackCD = 2f;
+
+    private bool _isAttack = false;
+    private bool _nextMovementState = true;
+    private bool _nextNomalAttack = true;
+
+    private bool ChasePlayer(float _targethaseDistance)
+    {
+
+        float distance = Vector3.Distance(transform.position, player.position);
+        if (distance > _targethaseDistance)
         {
-            _currentState = State.ChaseCircle;
-            ChaseCircleInitial();
-            StartCoroutine(CountNormalAttackCd());
+            if (_nextMovementState)
+                rb.velocity = (player.position - transform.position).normalized * moveSpeed;
+            return false;
         }
-
+        return true;
     }
-
-    private void ChaseCircleInitial()
-    {
-        Debug.Log("Start target");
-
-        // Complex random
-        _circleOffsetRadius = (float)Random.Range(8, 15) / 10 * circleRadius;
-        _isCircleCW = (Random.Range(0, 2) == 0) ? true : false;
-        StartCoroutine(CountCircleCWCd());
-
-        // Set angle direction [angle that player aim to enemy]
-        Vector2 distanceVector = transform.position - player.position;
-        _circleFaceAngle = Mathf.Atan2(distanceVector.y, distanceVector.x);
-    }
-
-    private float _chaseCircleCWCd = 2f;
-    private bool _nextCircleCWChange = false;
-
-    private IEnumerator CountCircleCWCd()
-    {
-        _nextCircleCWChange = false;
-        yield return new WaitForSeconds(_chaseCircleCWCd);
-        _nextCircleCWChange = true;
-    }
-
-    private float _normalAttackCd = 3f;
-    private bool _nextNormalAttack = true;
-    private IEnumerator CountNormalAttackCd()
-    {
-        _nextNormalAttack = false;
-        yield return new WaitForSeconds(_normalAttackCd);
-        _nextNormalAttack = true;
-    }
-
-    private void ChaseCircle()
-    {
-        // Calculate the next position on the orbit path
-        if (_isCircleCW)
-            _circleFaceAngle -= Time.fixedDeltaTime * circleAngularSpeed;
-        else
-            _circleFaceAngle += Time.fixedDeltaTime * circleAngularSpeed;
-        Debug.Log(_circleFaceAngle);
-
-        float x = Mathf.Cos(_circleFaceAngle) * _circleOffsetRadius;
-        float y = Mathf.Sin(_circleFaceAngle) * _circleOffsetRadius;
-
-        // Determine the target position based on the player's position and angle
-        Vector3 circlePosition = new Vector3(player.position.x + x, player.position.y + y, 0);
-        Vector3 direction = (circlePosition - transform.position).normalized;
-
-        if (_nextMovementState)
-            rb.velocity = direction * circleSpeed;
-
-        if (_nextCircleCWChange)
-        {
-            StartCoroutine(CountCircleCWCd());
-            _isCircleCW = !_isCircleCW;
-        }
-
-        if (Vector3.Distance(player.position, transform.position) < _circleOffsetRadius * 1.5 || _forceEnd)
-        {
-            Debug.Log("Arrived target");
-            _currentState = State.Idle;
-        }
-    }
-
 }
